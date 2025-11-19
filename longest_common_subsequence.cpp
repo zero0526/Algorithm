a50698c16311a1c1f0be5e0aa610204f7d20c539
khoe <nguyenkhoe2k4@gmail.com>
2025-11-20 01:42:46 +0700

    longest subsequence

--- longest_common_subsequence.cpp
+++ longest_common_subsequence.cpp
@@ -25,13 +25,25 @@ TEST CASES:
 */
 
 int longestCommonSubsequence(string text1, string text2) {
-    // TODO: Viết code của bạn ở đây
-    return -1;
+    // TODO: 
+    int n1= text1.size();
+    int n2= text2.size();
+    vector<vector<int>> dp(n1+1, vector<int>(n2+1, 0));
+    for(int i=1; i<=n1; i++){
+      for(int j=1;j<=n2;j++){
+         if(text1[i-1]==text2[j-1]){
+            dp[i][j]=dp[i-1][j-1]+1;
+         }else{
+            dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
+         }
+      }
+    }    
+    return dp[n1][n2];
 }
 
 int main() {
-    string s1 = "abcde";
-    string s2 = "ace";
+    string s1,s2;
+    cin >> s1 >> s2;
     cout << "LCS length: " << longestCommonSubsequence(s1, s2) << endl;
     return 0;
 }
